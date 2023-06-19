5f28166220dec170573f2b63cfdbc7b2a0a85ff6
Farhana Ahmed Fima <farhanaahmedfima@Farhanas-MBP.box>
2023-06-19 11:28:28 +0200

    add : data types in dart

--- variables_and_data_types/bin/data_types.dart
+++ variables_and_data_types/bin/data_types.dart
@@ -3,4 +3,21 @@ void main(List<String> args) {
   print(age);
   final double newAge = 30.5;
   print(newAge);
+  final String address = "1B Oudenarder Str. ";
+  print(address);
+  final bool flag = true;
+  print(flag);
+  print(!flag);
+  final List<int> num = [1, 2, 3, 4, 4];
+  print(num);
+  final List<String> names = ['ABC', 'abc', 'XYZ', 'xyz'];
+  print(names);
+  final Map details = {'username': 'abcxyz', 'password': 'pass@1234'};
+  print(details);
+  final Set uniqueNum = {1, 2, 3, 4};
+  print(uniqueNum);
+  dynamic variable = 30;
+  print(variable);
+  variable = "Thirty";
+  print(variable);
 }
