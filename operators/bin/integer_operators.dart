c6a5a8cf93dfe090d4ab7d008b842c6ff2c7cdb9
Farhana Ahmed Fima <farhanaahmedfima@Farhanas-MBP.box>
2023-06-19 20:20:45 +0200

    add : simple integer operators.

---
+++ operators/bin/integer_operators.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  const num1 = 80;
+  const num2 = 40;
+  print(num1 + num2);
+  print(num1 - num2);
+  print(num1 * num2);
+  print(num1 / num2);
+  print(num1 ~/ num2);
+  print(num1 % num2);
+
+  const num3 = 10.5;
+  const num4 = 9;
+  print(num3 / num4);
+  print(num3 ~/ num4);
+  print(num3 % num4);
+  print(4 % 5);
+}
